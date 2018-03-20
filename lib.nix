e0e92a74318c5f21903bd231856aa91fcfb590a4
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-03-20 13:28:20 +0000

    fixup! [DEVOPS-673] Pin cardano-sl git revision in one place
    Review comment: debugging code not needed.

--- lib.nix
+++ lib.nix
@@ -1,9 +1,5 @@
 let
-  # Allow overriding pinned nixpkgs for debugging purposes via cardano_pkgs
-  fetchNixPkgs = let try = builtins.tryEval <cardano_pkgs>;
-    in if try.success
-    then builtins.trace "using host <cardano_pkgs>" try.value
-    else import ./fetch-nixpkgs.nix;
+  fetchNixPkgs = import ./fetch-nixpkgs.nix;
 
   maybeEnv = env: default:
     let
